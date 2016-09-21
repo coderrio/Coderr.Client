886e45332b6b99c6db5a0cef4f4ed699972c9335
Eran Gil <eran.gil2@gmail.com>
2016-09-21 21:03:50 +0300

    Added a wpf demo project to be able to test the functionality of the wpf client

---
+++ client.wpf/OneTrueError.Client.Wpf.Demo/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace OneTrueError.Client.Wpf.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var url = new Uri("http://localhost/onetrueerror/");
+            OneTrue.Configuration.Credentials(url, "yourAppKey", "yourSharedSecret");
+            OneTrue.Configuration.CatchWpfExceptions();
+            OneTrue.Configuration.UserInteraction.AskUserForDetails = true;
+            OneTrue.Configuration.UserInteraction.AskUserForPermission = true;
+            OneTrue.Configuration.UserInteraction.AskForEmailAddress = true;
+//            Application.Current.DispatcherUnhandledException += CurrentOnDispatcherUnhandledException;
+            InitializeComponent();
+        }
+
+        private void ThrowException(object sender, RoutedEventArgs e)
+        {
+            throw new Exception("Demo exception");
+        }
+    }
+}
