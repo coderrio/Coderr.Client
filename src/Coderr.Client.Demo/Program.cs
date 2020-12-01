fbbbd1142e54a2ca8c110019eed7d3a9327a365c
Jonas Gauffin <jonas@gauffin.org>
2020-12-01 18:31:42 +0100

    icon

--- src/Coderr.Client.Demo/Program.cs
+++ src/Coderr.Client.Demo/Program.cs
@@ -6,12 +6,10 @@ namespace Coderr.Client.Demo
     {
         static void Main()
         {
-
             var url = new Uri("http://localhost:60473/");
             Err.Configuration.Credentials(url,
                 "1a68bc3e123c48a3887877561b0982e2",
                 "bd73436e965c4f3bb0578f57c21fde69");
-
 
             Err.ReportLogicError("User should have been assigned.");
             try
